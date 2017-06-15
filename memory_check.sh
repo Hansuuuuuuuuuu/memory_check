8357fcbc7a3ee2e9e0757c5b1e17c39d43e6ebd4
Hansuuuuuuuuuu <hanschristiancristobal@gmail.com>
2017-06-15 08:39:55 -0400

    figuring out float division with awk

--- memory_check.sh
+++ memory_check.sh
@@ -5,7 +5,9 @@ totalmem=$( free | grep Mem: | awk '{ print $2 }')
 freemem=$( free | grep Mem: | awk '{ print $4 }')
 
 
+usage=$( awk -v var1="$freemem" -v var2="$totalmem" 'BEGIN {print var1/var2}' )
 
-echo $totalmem
-echo
-echo $freemem
+
+
+
+echo $usage
