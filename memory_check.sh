f9d9bc67d2884b61f40a8c394edd3980c0157930
Hansuuuuuuuuuu <hanschristiancristobal@gmail.com>
2017-06-15 10:03:03 -0400

    Conditionals and exits

--- memory_check.sh
+++ memory_check.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]
-then
+if [ "$#" -ne 6 ]; then
   echo -e "Usage: \n-c <Critical Threshold> \n-w <Warning Threshold> \n-e <Email>"
   exit
 fi
-
-
-
-
-
-totalmem=$( free | grep Mem: | awk '{ print $2 }')
-usedmem=$( free | grep Mem: | awk '{ print $3 }')
-usage=$( awk -v var1="$usedmem" -v var2="$totalmem" 'BEGIN {print var1/var2}' )
-
-
 
 while getopts c:w:e: option
 do
@@ -26,10 +15,21 @@ do
  esac
 done
 
+if [ "$warning" -gt "$critical" ]; then
+	echo -e "Critical threshold must be greater than warning threshold"
+	echo -e "Usage: \n-c <Critical Threshold> \n-w <Warning Threshold> \n-e <Email>"
+	exit
+fi
 
-echo $usage
-echo $critical
-echo $warning
-echo $email
 
+totalmem=$( free | grep Mem: | awk '{ print $2 }')
+usedmem=$( free | grep Mem: | awk '{ print $3 }')
+usage=$( awk -v var1="$usedmem" -v var2="$totalmem" 'BEGIN {print var1/var2 * 100}' )
 
+if echo $usage $critical | awk '{exit !( $1 >= $2)}'; then
+	exit 2
+elif echo $usage $warning | awk '{exit !( $1 >= $2)}'; then
+	exit 1
+else
+	exit 0
+fi
