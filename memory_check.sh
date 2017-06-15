ef5f6cd1c165197c37bd6655243719135b576826
Hansuuuuuuuuuu <hanschristiancristobal@gmail.com>
2017-06-15 09:18:58 -0400

    fixed usage values

--- memory_check.sh
+++ memory_check.sh
@@ -2,12 +2,23 @@
 
 
 totalmem=$( free | grep Mem: | awk '{ print $2 }')
-freemem=$( free | grep Mem: | awk '{ print $4 }')
+usedmem=$( free | grep Mem: | awk '{ print $3 }')
 
 
-usage=$( awk -v var1="$freemem" -v var2="$totalmem" 'BEGIN {print var1/var2}' )
+usage=$( awk -v var1="$usedmem" -v var2="$totalmem" 'BEGIN {print var1/var2}' )
 
-
+while getopts c:w:e: option
+do
+ case "${option}"
+ in
+ c) critical=${OPTARG};;
+ w) warning=${OPTARG};;
+ e) email=${OPTARG};;
+ esac
+done
 
 
 echo $usage
+echo $critical
+echo $warning
+echo $email
